545e68b4b6aa7d2b1e995a092c32e5dc1ca169c5
zyongan <yonganzhong@outlook.com>
2019-09-09 09:25:28 +0800

    subject 2

---
+++ subject2/addTwoNumbers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @ File num_index.py
+# @ Description 求解两个链表之和
+# @ Author alexchung
+# @ Time 8/9/2019 AM 10:27
+
+"""
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+"""
+
+import os
+from subject2.singleLinkedList import ListNode
+
+# def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+if __name__ == "__main__":
+    l1 = ListNode(10)
+
+
+
+
